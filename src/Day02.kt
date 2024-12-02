7efbf7d74ffeb62136eb8cd0ae42b7a66406d4a8
Mikael Bärlund <mikael.barlund@iki.fi>
2024-12-02 18:10:33 +0200

    aoc2024-day2

---
+++ src/Day02.kt
@@ -0,0 +1,53 @@
+import kotlin.math.abs
+
+fun main() {
+    val file = "src/input/santa/Day02.txt"
+    Day02.day02(file)
+}
+
+class Day02 {
+    private data class Day(val safe: Boolean, val previous: Int?, val dampened: Boolean)
+    companion object {
+        fun day02(path: String) {
+            println(
+                "day 1 first: " +
+                        path.readFile()
+                            .split("\n")
+                            .map { it.split(" ").map { a -> a.toInt() } }
+                            .map { it.isSafe(false) }
+                            .count{it}
+            )
+            println(
+                "day 1 second: " +
+                        path.readFile()
+                            .split("\n")
+                            .map { it.split(" ").map { a -> a.toInt() } }
+                            .map { it.isSafe(true) || it.reversed().isSafe(true) }
+                            .count{it}
+            )
+        }
+
+        private fun List<Int>.isSafe(dampen: Boolean): Boolean {
+            val dir = this[0].dir(this[1])
+            val safe = this.foldIndexed(Day(true, null as Int?, false)) { i, acc, element ->
+                if (i == 0) return@foldIndexed Day(true, null, false)
+                val prev: Int = acc.previous ?: (i - 1)
+                val diff = abs(element - this[prev])
+                val thisDir = this[prev].dir(element)
+                val thisSafe = (
+                        acc.safe &&
+                                dir == thisDir &&
+                                diff < 4 &&
+                                diff > 0
+                        )
+                if (thisSafe) Day(true, null, acc.dampened)
+                else if (dampen &&!acc.dampened) Day(true, prev, true)
+                else Day(false, null, true)
+            }
+            return safe.safe
+        }
+        private fun Int.dir(b: Int): Boolean = this > b
+    }
+}
+
+
