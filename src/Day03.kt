4faa4f32659f723ad55a8826b6d0352a09d1880b
Mikael Bärlund <mikael.barlund@iki.fi>
2024-12-04 08:31:48 +0200

    aoc2024-day3

--- src/Day03.kt
+++ src/Day03.kt
@@ -5,14 +5,28 @@ fun main() {
 
 class Day03 {
     companion object {
+        private fun String.findmatches(regex: Regex): List<String> {
+            return regex.findAll(this).map { it.value }.toList()
+        }
+        private fun String.mul(): Int {
+            val (a, b) = this.findmatches(Regex("\\d{1,3}")).map { it.toInt() }
+            return a*b
+        }
         fun day03(path: String) {
             println(
-                "first: " +
+                "first: " + //168539636
                         path.readFile()
+                            .findmatches(Regex("mul\\(\\d{1,3},\\d{1,3}\\)"))
+                            .sumOf { it.mul() }
             )
             println(
-                "second: " +
+                "second: \n" + //97529391
                         path.readFile()
+                            .replace("\n", "")
+                            .findmatches(Regex("(^|do\\(\\)).*?($|don't\\(\\))"))
+                            .joinToString ()
+                            .findmatches(Regex("mul\\(\\d{1,3},\\d{1,3}\\)"))
+                            .sumOf { it.mul() }
             )
         }
     }
